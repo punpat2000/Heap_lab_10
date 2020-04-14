ec087c38bf536bd23347613fec8637c66da016fc
punpat2000 <saint12302@gmail.com>
2020-04-14 14:09:43 +0700

    update

--- src/MaxIntHeap.java
+++ src/MaxIntHeap.java
@@ -1,5 +1,26 @@
 
+import java.util.Random;
+
 public class MaxIntHeap extends Heap{
+	
+	public static void main(String[] args) throws Exception {
+		int[] x = new int[1000000]; //100, 10000, 1000000
+		for (int i = 0; i < x.length; x[i++] = new Random().nextInt());
+		
+		long startTime = System.nanoTime();
+		Heap h = new MaxIntHeap();
+		for (int i = 0; i < x.length; h.add(x[i++]));
+		long endTime = System.nanoTime();
+		System.out.println("Add time: " + (endTime - startTime));
+		
+		startTime = System.nanoTime();
+		while (!h.isEmpty())
+			h.pop();
+		endTime = System.nanoTime();
+		System.out.println("Pop time: " + (endTime - startTime));
+		
+	}
+	
 	@Override
 	public void add(Object element) {
 		// Add your code here
